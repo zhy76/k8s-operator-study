a23a1ad6ed7d83b85100fff8952e551f2c28bf1f
zhy76 <958474674@qq.com>
2023-02-19 11:24:19 +0000

    feat:add event logic
    Signed-off-by: zhy76 <958474674@qq.com>

--- client-go-study/client-go-demo/pkg/controller.go
+++ client-go-study/client-go-demo/pkg/controller.go
@@ -1,30 +1,57 @@
 package pkg
 
 import (
+	v12 "k8s.io/api/networking/v1"
+	v13 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
 	informer "k8s.io/client-go/informers/core/v1"
 	netInformer "k8s.io/client-go/informers/networking/v1"
 	"k8s.io/client-go/kubernetes"
 	coreLister "k8s.io/client-go/listers/core/v1"
 	v1 "k8s.io/client-go/listers/networking/v1"
 	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"reflect"
 )
 
 type controller struct {
 	client        kubernetes.Interface
 	ingressLister v1.IngressLister
 	serviceLister coreLister.ServiceLister
+	queue         workqueue.RateLimitingInterface
 }
 
 func (c *controller) addService(obj interface{}) {
-
+	c.enqueue(obj)
 }
 
 func (c *controller) updateService(oldObj interface{}, newObj interface{}) {
+	//todo 比较annotation是否相同
+	if reflect.DeepEqual(oldObj, newObj) {
+		return
+	}
+	c.enqueue(newObj)
+}
 
+func (c *controller) enqueue(obj interface{}) {
+	key, err := cache.MetaNamespaceIndexFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+	}
+
+	c.queue.Add(key)
 }
 
 func (c *controller) deleteIngress(obj interface{}) {
-
+	ingress := obj.(*v12.Ingress)
+	service := v13.GetControllerOf(ingress)
+	if service != nil {
+		return
+	}
+	if service.Kind != "Service" {
+		return
+	}
+	c.queue.Add(ingress.Namespace + "/" + ingress.Name)
 }
 
 func (c *controller) Run(stopCh chan struct{}) {
@@ -36,6 +63,7 @@ func NewController(client kubernetes.Interface, serviceInformer informer.Service
 		client:        client,
 		ingressLister: ingressInformer.Lister(),
 		serviceLister: serviceInformer.Lister(),
+		queue:         workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "ingressManager"),
 	}
 	serviceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    c.addService,
